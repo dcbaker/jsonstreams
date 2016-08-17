0c7b3e690563c96f6ca166ef7a65973db05760f5
Dylan Baker <dylan@pnwbakers.com>
2016-08-17 13:21:51 -0700

    Add docstrings and comments.

--- jsonstreams/__init__.py
+++ jsonstreams/__init__.py
@@ -19,6 +19,47 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+"""A streaming JSON writing library.
+
+JSON streams provides a simple, object oriented, interface for writing JSON
+data as a stream, without building a complex data tree first. This is
+advantageous when working with particularly large data sets that would consume
+a lot of memory to build out completely, or when using systems with constrained
+memory.
+
+The interface is designed to be as safe as possible, and provides context
+manager interfaces to maximize this safety.
+
+Basic usage:
+
+Create a Stream instance, which will be either an object (in JSON terminology,
+dict in python terminology), or an array (list in python terminology). Then use
+the write method to write elements into the file.
+
+>>> with Stream('foo', 'array') as s:
+...     s.write('foo')
+...     s.write('bar')
+
+Each element can also open a subelement, either via the subarray() or
+subobject() method.
+
+>>> with Stream('foo', 'array') as s:
+...     s.write('foo')
+...     with s.subobject() as o:
+...         o.write(1, 'bar')
+
+Any object that can be serialized can be passed (although passing some elements
+as object keys is not supported, and should not be passed).
+
+>>> with Stream('foo', 'object') as s:
+...     s.write('foo', {'foo': 'bar'})
+
+It is very important to note that while the Array and Object classes present
+the same API, the signature of their write methods is necessarily different.
+For Array, it accepts a single element, for Object it requires two elements,
+the key and the value.
+"""
+
 import copy
 import functools
 try:
@@ -32,6 +73,7 @@ __all__ = [
 
 
 class Open(object):
+    """A helper to allow subelements to be used as context managers."""
 
     def __init__(self, initializer, writer=None, key=None):
         if writer:
@@ -52,6 +94,7 @@ class Open(object):
 
 
 class Object(object):
+    """A streaming array representation."""
 
     def __init__(self, fd, indent, baseindent, encoder, _indent=True):
         self.__fd = fd
@@ -61,7 +104,11 @@ class Object(object):
         self.__baseindent += 1
         self.__encoder = encoder
 
+        # This does a rather clever hack to make the comma write free of ifs
+        # using polymorphism. Basically after the method below writes, and then
+        # replaces it self with a version that adds a comma.
         self.write = self.__write_no_comma
+
         self.subobject = functools.partial(
             Open,
             functools.partial(Object, self.__fd, self.__indent,
@@ -82,15 +129,20 @@ class Object(object):
             self.__fd.write('\n')
 
     def __write_key(self, key):
-        self.__write(self.__encoder.encode(key), indent=True)
+        self.__write(self.__encoder.encode(key), indent=self.__indent)
         self.__write(': ')
 
     def __write_no_comma(self, key, value):
+        """Write without a comma."""
         self.__write_key(key)
         self.__write(self.__encoder.encode(value))
+
+        # replace with the comma version, removeing the need for extra if
+        # statements.
         self.write = self.__write_comma
 
     def __write_comma(self, key, value):
+        """Write with a comma."""
         if not self.__indent:
             self.__write(', ')
         else:
@@ -112,6 +164,7 @@ class Object(object):
 
 
 class Array(object):
+    """A streaming array representation."""
 
     def __init__(self, fd, indent, baseindent, encoder, _indent=True):
         self.__fd = fd
@@ -151,10 +204,11 @@ class Array(object):
         self.__write(self.__encoder.encode(value), indent=self.__indent)
 
     def close(self):
+        """Close the group."""
         if self.__indent:
             self.__write('\n')
         self.__baseindent -= 1
-        self.__write(']', indent=True)
+        self.__write(']', indent=self.__indent)
 
     def __enter__(self):
         return self
@@ -164,6 +218,30 @@ class Array(object):
 
 
 class Stream(object):
+    """A JSON stream object.
+
+    This object is the "root" object for the stream. It handles opening and
+    closing the file, and provides the ability to write into the stream, and to
+    open sub elements.
+
+    This element can be used as a context manager, which is the recommended way
+    to use it.
+
+    Arguments:
+    filename -- the name of the file to open
+    jtype    -- either 'object' or 'array', this will set the type of the
+                stream's root.
+
+    Keyword Arguments:
+    indent   -- How much to indent each level, if set to 0 no indent will be
+                used and the stream will be written in a single line.
+                Default: 0
+    encoder  -- A json.JSONEncoder instance. This will be used to encode
+                objects passed to the write method of the Stream and all
+                instances returned by the subarray and subobject methods of
+                this instance and it's children.
+                Default: json.JSONEncoder
+    """
 
     _types = {
         'object': Object,
@@ -183,6 +261,7 @@ class Stream(object):
         self.write = self.__inst.write
 
     def close(self):
+        """Close the root element and the file."""
         self.__inst.close()
         self.__fd.close()
 
