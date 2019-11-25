60a604394807af6bd0ee367b5de1821ee1c2f782
Dylan Baker <dylan@pnwbakers.com>
2019-11-25 15:27:01 -0800

    setup.py: use the python_requires keyword

--- setup.py
+++ setup.py
@@ -69,5 +69,6 @@ setup(
     },
     keywords='JSON stream',
     packages=['jsonstreams'],
-    cmdclass={'test': Tox}
+    cmdclass={'test': Tox},
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
 )
